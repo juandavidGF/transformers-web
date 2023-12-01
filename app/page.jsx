1accbcc688f8e4ff8bec456e7656d3fcac13f7cd
juandavidGF <davad701@gmail.com>
2023-12-01 03:54:49 -0500

    snt analysis web

--- app/page.jsx
+++ app/page.jsx
@@ -47,6 +47,7 @@ export default function Home() {
       worker.current.postMessage({ text });
     }
   }, []);
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-12">
       <h1 className="text-5xl font-bold mb-2 text-center">Transformers.js</h1>
